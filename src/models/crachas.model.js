c9f3cddd4e8992c96c0b88909a13464ed61b8daf
Murilo Henrique <77072462+murilo-henrique060@users.noreply.github.com>
2023-11-30 20:19:14 +0000

    modelo da tabela de crachás finalizado

---
+++ src/models/crachas.model.js
@@ -0,0 +1,56 @@
+/*
+  crachas.model.js
+
+  Esse arquivo é responsável pelo modelo da tabela de crachás do banco de dados.
+
+  Requisito:
+
+  Último Editor: Murilo
+
+  Status: Finalizado
+
+  Notas:
+*/
+
+const database = require('../services/database.service.js');
+const { DataTypes } = require('sequelize');
+
+const Cursos = require('./cursos.model.js');
+const Usuarios = require('./usuarios.model.js');
+
+const Crachas = database.define('crachas', {
+  'id_cracha': {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  'nome_cracha': {
+    type: DataTypes.STRING(45),
+    allowNull: false
+  },
+  'matricula_cracha': {
+    type: DataTypes.STRING(11),
+    allowNull: false
+  },
+  'foto_cracha': {
+    type: DataTypes.BLOB('long'),
+    allowNull: true
+  },
+  'imagem_cracha': {
+    type: DataTypes.BLOB('long'),
+    allowNull: false
+  },
+  'id_curso': {
+    type: 'INT',
+    allowNull: false
+  },
+  'id_usuario': {
+    type: 'INT',
+    allowNull: false
+  }
+}, {});
+
+Crachas.belongsTo(Cursos, { foreignKey: 'id_curso' });
+Crachas.belongsTo(Usuarios, { foreignKey: 'id_usuario' });
+
+module.exports = Crachas;
---
+++ test/unit/models/crachas.model.test.js
@@ -0,0 +1,30 @@
+/*
+  crachas.model.test.js
+
+  Esse arquivo testa o modelo da tabela de crachás do banco de dados.
+
+  Requisito:
+
+  Último Editor: Murilo
+
+  Status: Finalizado
+
+  Notas:
+*/
+
+require('dotenv').config({ path: './src/configs/.env' });
+
+const { test } = require('@jest/globals');
+const Crachas = require('../../../src/models/crachas.model.js');
+
+test('Modelo da Tabela de Cursos', async () => {
+  let table = await Crachas.describe();
+
+  expect(table).toHaveProperty('id_cracha');
+  expect(table).toHaveProperty('nome_cracha');
+  expect(table).toHaveProperty('matricula_cracha');
+  expect(table).toHaveProperty('foto_cracha');
+  expect(table).toHaveProperty('imagem_cracha');
+  expect(table).toHaveProperty('id_curso');
+  expect(table).toHaveProperty('id_usuario');
+});
