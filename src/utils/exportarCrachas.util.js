74b9dbd9e917ee94dfb569b77148d189e3288d91
Murilo Henrique <77072462+murilo-henrique060@users.noreply.github.com>
2023-12-20 14:18:15 +0000

    puppeter launch fixed

--- src/utils/exportarCrachas.util.js
+++ src/utils/exportarCrachas.util.js
@@ -65,7 +65,7 @@ const exportarPDF = async (crachas) => {
 
   const html = gerarHtml(crachas);
     
-  const browser = await puppeteer.launch({ headless: 'new' });
+  const browser = await puppeteer.launch({ headless: 'new', args: ['--no-sandbox', '--disable-setuid-sandbox'] });
   const page = await browser.newPage();
   await page.setContent(html);
   await page.pdf(options);
